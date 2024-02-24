d1ea03ac33497a358c5ae6b0ed2f1bf616ff73da
Jörg Jooss <joerg@joergjooss.de>
2024-02-24 14:15:03 +0100

    Add pull based refresh

--- AzureAppConfigDemo/Program.cs
+++ AzureAppConfigDemo/Program.cs
@@ -1,11 +1,28 @@
+using System.Reflection.Emit;
 using AzureAppConfigDemo;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("AppConfig");
-builder.Configuration.AddAzureAppConfiguration(connectionString);
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    options
+        .Connect(connectionString)
+        .Select("TestApp:*", LabelFilter.Null)
+        .ConfigureRefresh(refreshOptions =>
+        {
+            // Default cache expiration is 30 seconds, lowering it for demo purposes.
+            refreshOptions
+                .Register("TestApp:Settings:Sentinel", refreshAll: true)
+                .SetCacheExpiration(TimeSpan.FromSeconds(15));
+        });
+});
+
 builder.Services.AddRazorPages();
+// Add App Configuration middleware.
+builder.Services.AddAzureAppConfiguration();
 
 builder.Services.Configure<Settings>(builder.Configuration.GetSection("TestApp:Settings"));
 
@@ -18,6 +35,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Enable middleware to trigger request driven refresh.
+app.UseAzureAppConfiguration();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
