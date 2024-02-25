28c7f39a5cd054de7d8f4c4bdbdc026c810517ee
Jörg Jooss <joerg@joergjooss.de>
2024-02-25 16:05:22 +0100

    Use snapshot

--- AzureAppConfigDemo/Program.cs
+++ AzureAppConfigDemo/Program.cs
@@ -1,34 +1,18 @@
 using AzureAppConfigDemo;
-using Microsoft.Extensions.Configuration.AzureAppConfiguration;
-using Microsoft.FeatureManagement;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("AppConfig");
+var snapshot = builder.Configuration.GetValue<string>("Snapshot");
 builder.Configuration.AddAzureAppConfiguration(options =>
 {
     options
         .Connect(connectionString)
-        .Select("TestApp:*", LabelFilter.Null)
-        .Select("TestApp:*", builder.Environment.EnvironmentName)
-        .ConfigureRefresh(refreshOptions =>
-        {
-            // Default cache expiration is 30 seconds, lowering it for demo purposes.
-            refreshOptions
-                .Register("TestApp:Settings:Sentinel", refreshAll: true)
-                .SetCacheExpiration(TimeSpan.FromSeconds(15));
-        })
-        .UseFeatureFlags();
+        .SelectSnapshot(snapshot);
 });
 
 builder.Services.AddRazorPages();
-// Add App Configuration middleware.
-builder.Services.AddAzureAppConfiguration();
-
-// Add feature management services.
-builder.Services.AddFeatureManagement();
-
 
 builder.Services.Configure<Settings>(builder.Configuration.GetSection("TestApp:Settings"));
 
@@ -41,9 +25,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-// Enable middleware to trigger request driven refresh.
-app.UseAzureAppConfiguration();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
--- AzureAppConfigDemo/Settings.cs
+++ AzureAppConfigDemo/Settings.cs
@@ -2,8 +2,8 @@ namespace AzureAppConfigDemo;
 
 public class Settings
 {
-    public string BackgroundColor { get; set; }
+    public string? BackgroundColor { get; set; }
     public long FontSize { get; set; }
-    public string FontColor { get; set; }
-    public string Message { get; set; }
+    public string? FontColor { get; set; }
+    public string? Message { get; set; }
 }
